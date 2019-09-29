742f7c5efb5e73a63898965b99845e3651da1c91
mathieumatos <mathieumatos1203@gmail.com>
2019-09-29 02:10:09 -0400

    to do : evaluate score from pairwise sequences

--- tp1-q1.py
+++ tp1-q1.py
@@ -25,14 +25,15 @@ def findMaxScore(i,j):
     up = tab[i-1][j] + indelValue
     left = tab[i][j-1] + indelValue
     maximum = max(diag, up, left)
-    maxOrigins = []
-    if maximum == diag:
-        maxOrigins.append([i-1,j-1])
     if maximum == up:
-        maxOrigins.append([i-1,j])
-
+        maxOrigins = 1
+        # maxOrigins.append([i-1,j])
     if maximum == left:
-        maxOrigins.append([i,j-1])
+        maxOrigins = -1
+        # maxOrigins.append([i,j-1])
+    if maximum == diag:
+        maxOrigins = 0
+        # maxOrigins.append([i-1,j-1])
 
     backtrackTab[i][j] = maxOrigins
     
@@ -42,13 +43,15 @@ def findMaxScore(i,j):
 ######### init du tableau 2d ##########
 
 tab = np.empty((rows,columns), np.int32)
-backtrackTab = np.empty((rows,columns), dtype=list)
+backtrackTab = np.empty((rows,columns), np.int32)
 
 for i in range(rows):
     tab[i][0] = 0
+    backtrackTab[i][0] = 1
 
 for j in range(columns):
     tab[0][j] = 0
+    backtrackTab[0][j] = -1
 
 #########################################
 
@@ -125,8 +128,58 @@ print(maxIndice)
 width = maxIndice[0]
 depth = maxIndice[1]
 
-while (width != 0) and (depth != 0):
-    
+seqS = ""
+seqT = ""
+
+# while ((width != -1) and (depth != -1)):
+#     print(str(width)+", "+str(depth))
+#     if
+
+while ((width > 0) and (depth > 0)):
+    print(str(width)+", "+str(depth))
+    if backtrackTab[width][depth] == 0:
+        seqS = S[width-1] + seqS
+        seqT = T[depth-1] + seqT
+        width += -1
+        depth += -1
+        continue
+    if backtrackTab[width][depth] == 1:
+        seqS = "-" + seqS
+        seqT = T[depth-1] + seqT
+        width += -1
+        continue
+    if backtrackTab[width][depth] == -1:
+        seqS = S[width-1] + seqS
+        seqT = "-" + seqT
+        depth += -1
+        continue
+
+print("jusqua 0")
+print(seqS)
+print(seqT)
+
+
+tempS = ""
+tempT = ""
+
+if width > 0:
+    for x in range(width):
+        tempS += S[x]
+        tempT += "-"
+
+if depth > 0:
+    for x in range(depth):
+        tempS += "-"
+        tempT += T[x]
+
+seqS = tempS + seqS
+seqT = tempT + seqT
+
+for x in range()
+
+print(str(width)+", "+str(depth))
+print(seqS)
+print(seqT)
 
 
 ######################
@@ -137,5 +190,20 @@ print(T)
 print(tab)
 print(backtrackTab)
 
-print(len(backtrackTab[8][6]))
-print(backtrackTab[8][6][1][1])
+# print(len(backtrackTab[8][6]))
+# print(backtrackTab[8][6][1][1])
+
+
+jo = 5
+ni = 3
+
+while ((jo != -1) and (ni != -1)):
+    print(str(jo)+", "+str(ni))
+    if(jo == 0):
+        ni += -1
+    elif(ni == 0):
+        jo += -1
+    else:
+        jo += -1
+        ni += -1
+
