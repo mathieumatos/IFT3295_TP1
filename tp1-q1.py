aa08c6c92b63387f016bad56bd7b04a47bf5266c
mathieumatos <mathieumatos1203@gmail.com>
2019-09-28 22:11:32 -0400

    missing retracing to find overlapping sequences scores

--- tp1-q1.py
+++ tp1-q1.py
@@ -24,14 +24,25 @@ def findMaxScore(i,j):
     diag = tab[i-1][j-1] + matchScore(i,j)
     up = tab[i-1][j] + indelValue
     left = tab[i][j-1] + indelValue
+    maximum = max(diag, up, left)
+    maxOrigins = []
+    if maximum == diag:
+        maxOrigins.append([i-1,j-1])
+    if maximum == up:
+        maxOrigins.append([i-1,j])
+
+    if maximum == left:
+        maxOrigins.append([i,j-1])
+
+    backtrackTab[i][j] = maxOrigins
     
-    return max(diag, up, left)
+    return maximum
 
 
 ######### init du tableau 2d ##########
 
 tab = np.empty((rows,columns), np.int32)
-backtrackTab = np.empty((rows,columns), np.int32)
+backtrackTab = np.empty((rows,columns), dtype=list)
 
 for i in range(rows):
     tab[i][0] = 0
@@ -52,9 +63,79 @@ for x in range(rows-1):
 
 
 
+### Trouver max de la derniere colonne ###
 
+# je sais, ce n'est pas la meilleure pratique, je suis nouveau avec python, je pourrais mettre -inf à la place
+tempMaxCol = -99999999
+indiceLastCol = -1
+for x in range(i): 
+    if tempMaxCol <= tab[x][len(T)]:
+        tempMaxCol = tab[x][len(T)]
+        indiceLastCol = [x,len(T)]
+
+
+print(tempMaxCol)
+print(indiceLastCol)
+###########################################
+
+
+
+### Trouver max de la derniere colonne ###
+
+# je sais, ce n'est pas la meilleure pratique, je suis nouveau avec python, je pourrais mettre -inf à la place
+tempMaxRow = -99999999
+indiceLastRow = -1
+for x in range(j): 
+    if tempMaxRow <= tab[len(S)][x]:
+        tempMaxRow = tab[len(S)][x]
+        indiceLastRow = [len(S),x]
+
+
+print(tempMaxRow)
+print(indiceLastRow)
+
+###########################################
+
+
+
+########## Max Value et son Indice #######
+
+maxLast = max(tempMaxCol, tempMaxRow)
+if tempMaxCol > tempMaxRow:
+    maxLast = tempMaxCol
+    maxIndice = indiceLastCol
+elif tempMaxCol < tempMaxRow:
+    maxLast = tempMaxRow
+    maxIndice = indiceLastRow
+else:
+    maxLast = tempMaxRow
+    if indiceLastCol[1] > indiceLastRow[0]:
+        maxIndice = indiceLastCol
+    else:
+        maxIndice = indiceLastRow
+
+print(maxIndice)
+
+############################################
+
+
+
+####### RETRACE ######
+
+width = maxIndice[0]
+depth = maxIndice[1]
+
+while (width != 0) and (depth != 0):
+    
+
+
+######################
 
 
 print(S)
 print(T)
 print(tab)
+print(backtrackTab)
+
+print(len(backtrackTab[8][6]))
+print(backtrackTab[8][6][1][1])
